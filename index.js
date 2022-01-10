9fd8175d2c713af119c1c034b7c37593cb5f9aea
Yeohoon <yhoon.yoon@gmail.com>
2022-01-10 12:49:21 -0500

    commit tree 4a0a38bf4c3bbd2672305a8dce85c6474073d715

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+let distanceFromHqInBlocks = function(start){
+  let destination = 42;
+  return Math.abs(start - destination);
+} 
+
+let distanceFromHqInFeet = function (start){
+  let destination = 42;
+  return Math.abs(start - destination) * 264;
+}
+
+let distanceTravelledInFeet = function (start, destination){
+  return Math.abs(start - destination) * 264;
+}
+
+let calculatesFarePrice = function (start, destination){
+  let difference = Math.abs(start - destination) * 264;
+    if ( difference <= 400){
+      return 0;
+    }
+    else if( (difference > 400) && (difference < 2000) ){
+      return (difference - 400) * 0.02; 
+    }
+   else if ( (difference > 2000) && (difference < 2500) ){
+     return 25;
+   }
+   else if ( difference > 2500){
+     return 'cannot travel that far';
+   }
+  
+}
